6a2a9fdb69251da36494b90a6cd4440903960eca
maitue <44012114+maitue@users.noreply.github.com>
2023-02-03 09:32:09 +0700

    add some package

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
