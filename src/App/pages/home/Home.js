587af3e38d47e2e5464b1f8d5ae90aaa04c7f47c
maitue <44012114+maitue@users.noreply.github.com>
2023-03-15 15:52:13 +0700

    add menu bar

--- src/App/pages/home/Home.js
+++ src/App/pages/home/Home.js
@@ -1,17 +1,9 @@
 import React, {Suspense, lazy} from "react";
 import { Switch ,Route} from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 
 const Tutorial = lazy(() => import('../javascript/Tutorial'));
-const useStyles = makeStyles((theme) => ({
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-    },
-}))
+
 export default function Home() {
-    const classes = useStyles();
     return (
         <Suspense fallback={<div> Loading....</div>}>
             <Switch>
